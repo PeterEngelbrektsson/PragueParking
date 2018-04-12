d873aea78907c3354420cb93ca71fa1ceb18e2f8
Peter Engelbrektsson <peter.engelbrektsson@hotmail.com>
2018-04-12 13:35:19 +0200

    Addad a partial class

--- ParkingPlace/Parking.cs
+++ ParkingPlace/Parking.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ParkingPlace
 {
-    public class Parking
+    public partial class Parking
     {
 
         public static void DisplayMenu(string[] parkingPlace, VehicleType vehicleType)
@@ -170,71 +170,8 @@ namespace ParkingPlace
             VehicleType type = GetVehicleTypeOfParkedVehicle(parkingPlace, oldPosition, registrationNumber);
             Move(parkingPlace, registrationNumber, type, oldPosition, newPosition);
         }
-        public static VehicleType GetVehicleTypeOfParkedVehicle(string[] parkingPlace, int position, string registrationNumber)
-        {
-            VehicleType type;
-            if (parkingPlace[position] == null)
-            {
-                throw new ParkingSpaceIsEmptyException();
-            }
-            else
-            {
-                int positionOfColon = parkingPlace[position].IndexOf(':');
-                // : means it's one or two Mc
-                if (positionOfColon >-1)
-                {
-                    type = VehicleType.Mc;
-                }
-                else
-                {
-                    // All strings are considered cars.
-                    type = VehicleType.Car;
-                }
-            }
-            return type;
-        }
-        public static int FindFirstSingleParkedMc(string[] parkingPlace, int startPosition)
-        {
-            if (startPosition > (parkingPlace.Length - 1))
-            {
-                throw new ArgumentException();
-            }
-            if (startPosition < 0)
-            {
-                throw new ArgumentException();
-            }
-
-            for (int i = startPosition; i < parkingPlace.Length; i++)
-            {
-                if (ParkingSlot.CountMc(parkingPlace[i]) == 1)
-                {
-                    return i;
-                }
-            }
-            // No single parked mc found => returning -1
-            return -1;
-        }
-        public static int FindLastSingleParkedMc(string[] parkingPlace, int startPosition)
-        {
-            if (startPosition > (parkingPlace.Length - 1))
-            {
-                throw new ArgumentException();
-            }
-            if (startPosition < 0)
-            {
-                throw new ArgumentException();
-            }
-
-            for (int i = startPosition; i >= 0; i--)
-            {
-                if (ParkingSlot.CountMc(parkingPlace[i]) == 1)
-                {
-                    return i;
-                }
-            }
-            // No single parked mc found => returning -1
-            return -1;
-        }
+    
+    
         public static void Optimize(string[] parkingPlace)
         {
             bool found;
@@ -257,45 +194,7 @@ namespace ParkingPlace
                 //  return -1 from search functions means that the search has reached the end or start of the array => exit optimize loop
             } while (found && (firstSingleMcPosition != -1 && lastSingleMcPosition != -1));
         }
-        public static void AddMcAtPosition(string[] parkingPlaces, string registrationNumber, int newPosition)
-        {
-            if (newPosition < 0)
-            {
-                throw new ArgumentException();
-            }
-            if (newPosition > (parkingPlaces.Length - 1))
-            {
-                throw new ArgumentException();
-            }
-            if (string.IsNullOrEmpty(registrationNumber))
-            {
-                throw new ArgumentException();
-            }
-
-            int numberOfMcAtNewPosition = ParkingSlot.CountMc(parkingPlaces[newPosition]);
-            if (numberOfMcAtNewPosition == 0 && parkingPlaces[newPosition] != null)
-            {
-                throw new ParkingPlaceOccupiedException("The new place to move the MC to is occupied by a car.");
-            }
-            if (numberOfMcAtNewPosition == 2)
-            {
-                throw new ParkingPlaceOccupiedException("The new place to move the MC to is occupied by two MCs.");
-            }
-            if (parkingPlaces[newPosition] == null)
-            {
-                parkingPlaces[newPosition] = ":" + registrationNumber;
-            }
-            else
-            {
-                if (ParkingSlot.CountMc(parkingPlaces[newPosition]) != 1)
-                {
-                    throw new DataMisalignedException("The parkingplace should contain one Mc but doesn't.");
-                }
-                // Add registration numnber at left side of sign
-                parkingPlaces[newPosition] = registrationNumber + parkingPlaces[newPosition];
-            }
-
-        }
+   
         public static void Move(string[] parkingPlaces, string registrationNumber, VehicleType vehicleType, int oldPosition, int newPosition)
         {
             if (oldPosition < 0)
