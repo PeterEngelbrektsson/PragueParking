015f52ce7c62c9a3955ae0183720cd29369effdd
Peter Engelbrektsson <peter.engelbrektsson@hotmail.com>
2018-04-12 15:07:01 +0200

    Call to Display menu in main

--- PragueParking/Program.cs
+++ PragueParking/Program.cs
@@ -12,29 +12,30 @@ namespace PragueParking
     {
         public const int NumberOfParkinPlaces = 100;
 
-        static void DisplayMenu()
-        {
-
-        }
-
+   
         public static void Main(string[] args)
         {
             //Main file
 
             // String array with 100 elements of parking Temp
-            string[] parkingPlace = new string[100];
+             string[] parkingPlace = new string[100];
+            /*
+             //add vehicle temporary
+             String vehicle = "asb123";
+             // Calling the find method 
+             parkingPlace[10] = "asb123";
+             int pos;
+             //pos=Parking.Find(parkingPlace, vehicle);
+             pos = Parking.Find(parkingPlace, vehicle);
+             // Remove 
+             Parking.Remove(parkingPlace, vehicle);
 
-            //add vehicle temporary
-            String vehicle = "asb123";
-            // Calling the find method 
-            //parkingPlace[10] = "asb123";
-            int pos;
-            //pos=Parking.Find(parkingPlace, vehicle);
-            //pos = Parking.Find(parkingPlace, vehicle);
-            // Remove 
-            Parking.Remove(parkingPlace, vehicle);
+             Console.WriteLine("Your pos is :- " + pos);
+             */
+            parkingPlace[10] = "ABC123";
 
-            //Console.WriteLine("Your pos is :- " + pos);
+            Parking.DisplayMenu(parkingPlace,VehicleType.Car);
+            Console.ReadLine();
         }
     }
 }
