d97cc2998e779ff84b2368eebcd4fd11b82b1c49
anders.librand@gmail.com <anders.librand@gmail.com>
2018-04-12 13:47:08 +0200

    Added bugfixes to Display Menu

--- ParkingPlace/Parking.cs
+++ ParkingPlace/Parking.cs
@@ -14,6 +14,7 @@ namespace ParkingPlace
         {
             // Console.Clear(); -- Do we want to clear screen between repeat displays of the menu or not ? 
 
+            Console.WriteLine();
             Console.WriteLine("  Prague Parking v1.0");
             Console.WriteLine("-----------------------");
             Console.WriteLine("1. Add a car"); 
@@ -33,25 +34,25 @@ namespace ParkingPlace
                 case 1: // Add a car
 
                     Console.WriteLine("Please enter the registration number of the vehicle : ");
-
                     registrationNumber = Console.ReadLine();
 
                     vehicleType = VehicleType.Car;
 
-                    Add(parkingPlace, registrationNumber, vehicleType);
+                    int position = Add(parkingPlace, registrationNumber, vehicleType);
 
+                    Console.WriteLine("Your vehicle has been parked at place number {0}.", position + 1); 
                     break;
 
                 case 2: // Add a motorcycle
 
                     Console.WriteLine("Please enter the registration number of the vehicle : ");
-
                     registrationNumber = Console.ReadLine();
                     
                     vehicleType = VehicleType.Mc;
 
-                    Add(parkingPlace, registrationNumber, vehicleType);
+                    position = Add(parkingPlace, registrationNumber, vehicleType);
 
+                    Console.WriteLine("Your vehicle has been parked at place number {0}.", position + 1);
                     break;
 
                 case 3: // Move a vehicle
@@ -59,25 +60,21 @@ namespace ParkingPlace
                     int newPosition = FindFreePlace(parkingPlace, registrationNumber, vehicleType );
 
                     Move(parkingPlace, registrationNumber, newPosition);
-
                     break;
 
                 case 4: // Find a vehicle
 
                     Console.WriteLine("Please enter the registration number of the vehicle : ");
-
                     registrationNumber = Console.ReadLine();
 
-                    int position = Find(parkingPlace, registrationNumber);
+                    position = Find(parkingPlace, registrationNumber);
 
                     Console.WriteLine("Your vehicle is parked at spot number {0}, at array value {1}.", position + 1, position); // Parking spots numbered 1 - 100 !
-
                     break;
 
                 case 5: // Remove a vehicle
 
                     Console.WriteLine("Please enter the registration number of the vehicle : ");
-
                     registrationNumber = Console.ReadLine();
 
                     Console.WriteLine("Please specify if your vehicle is a car or an mc : ");
@@ -99,6 +96,7 @@ namespace ParkingPlace
                         throw new ArgumentException(); // Neither nor, then throw exception !
                     }
 
+                    Remove(parkingPlace, registrationNumber);
                     break;
 
                 case 6: // Find free parking spot
@@ -126,20 +124,17 @@ namespace ParkingPlace
 
                     position = FindFreePlace(parkingPlace, registrationNumber, vehicleType);
 
-                    Console.WriteLine("There is a free place for your vehicle at location {0}.", position + 1);
-
+                    Console.WriteLine("There is a free place for your vehicle at location {0}, at array value {1}.", position + 1, position);
                     break;
 
                 case 7: // Optimize parking spot
 
                     Optimize(parkingPlace);
-
                     break;
 
                 default: // None of the above, throw exception !
 
                     throw new ArgumentException();
-
                     break;
             }
         }
