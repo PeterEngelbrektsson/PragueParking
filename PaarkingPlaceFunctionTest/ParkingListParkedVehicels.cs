6f933f526a67a713b30b9b31762a26877c9850f3
Peter Engelbrektsson <peter.engelbrektsson@hotmail.com>
2018-04-13 10:33:33 +0200

    Added testcase

--- PaarkingPlaceFunctionTest/ParkingListParkedVehicels.cs
+++ PaarkingPlaceFunctionTest/ParkingListParkedVehicels.cs
@@ -32,7 +32,7 @@ namespace PaarkingPlaceFunctionTest
         }
 
         [TestMethod]
-        public void ListParkedVehiclesNullaTest()
+        public void ListParkedVehiclesNullTest()
         {
             // Setup
             string[] park = new string[5];
@@ -46,6 +46,38 @@ namespace PaarkingPlaceFunctionTest
             expected.Add(0, ":lk433");
             expected.Add(1, "dbc423:lk433");
             expected.Add(4, "dbt423:ltt33");
+
+            Dictionary<int, string> actual;
+
+            //Act
+            actual = Parking.ListParkedVehicels(park);
+
+            //Verify
+            MyAssert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void ListParkedVehicles10Test()
+        {
+            // Setup
+            string[] park = new string[10];
+            park[0] = ":lk433";
+            park[1] = "dbc423:lk433";
+            park[2] = null;
+            park[3] = null;
+            park[4] = "dbt423:ltt33";
+            park[5] = null;
+            park[6] = ":mcnr1";
+            park[7] = null;
+            park[8] = null;
+            park[9] = "car123";
+
+            Dictionary<int, string> expected = new Dictionary<int, string>();
+            expected.Add(0, ":lk433");
+            expected.Add(1, "dbc423:lk433");
+            expected.Add(4, "dbt423:ltt33");
+            expected.Add(6, ":mcnr1");
+            expected.Add(9, "car123");
 
             Dictionary<int, string> actual;
 
